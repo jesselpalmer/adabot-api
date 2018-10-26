b214193d5d8f8d3f425a6e2905c5fcb4da672708
Jesse Palmer <jesselpalmer@gmail.com>
2018-10-26 16:29:34 -0700

    refactor: separate quote data

---
+++ functions/src/cs-quotes/cs-quotes.data.ts
@@ -0,0 +1,12 @@
+import { CSQuote } from './cs-quotes'
+
+export const CSQuotesData: Array<CSQuote> = [
+  {
+    text: 'Computer science is no more about computers than astronomy is about telescopes.', 
+    author: 'Edsger Dijkstra'
+  },
+  {
+    text: 'Never trust a computer you can\'t throw out a window.',
+    author: 'Steve Wozniak'
+  }
+]
--- functions/src/cs-quotes/cs-quotes.service.ts
+++ functions/src/cs-quotes/cs-quotes.service.ts
@@ -1,22 +1,11 @@
 import { CSQuote } from './cs-quotes'
+import { CSQuotesData } from './cs-quotes.data'
 import { SlackMessage } from '../shared/slack-message'
 
 export class CSQuotesService {
-
-  quotes:Array<CSQuote> = [
-    {
-      text: 'The unexamined life is not worth living.', 
-      author: 'Socrates'
-    },
-    {
-      text: 'The journey of a thousand miles begins with one step.',
-      author: 'Lao Tzu'
-    }
-  ]
-
   getRandomCsQuote(): CSQuote {
-    const randomNum = Math.floor(Math.random() * this.quotes.length)
-    return this.quotes[randomNum]
+    const randomNum = Math.floor(Math.random() * CSQuotesData.length)
+    return CSQuotesData[randomNum]
   }
 
   getCSQuote(): SlackMessage {
