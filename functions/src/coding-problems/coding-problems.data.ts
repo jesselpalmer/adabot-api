5ec5935273e2eac233ee0f210a1e249cdc0c5b46
Jesse Palmer <jesselpalmer@gmail.com>
2018-10-26 16:56:27 -0700

    fix: update strings

--- functions/src/coding-problems/coding-problems.data.ts
+++ functions/src/coding-problems/coding-problems.data.ts
@@ -5,7 +5,7 @@ export const CodingProblemData: Array<CodingProblem> = [
     desc: 'Most frequently asked question',
     level: 'easy',
     question: 'Write a function that takes in a string and returns the most frequently used char ignoring whitespace.',
-    input: 'Today is the greatest day ever!',
-    output: 'e'
+    input: '\'Today is the greatest day ever!\'',
+    output: '\'e\''
   }
 ]
